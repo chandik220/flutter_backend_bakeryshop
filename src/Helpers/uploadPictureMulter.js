9f9e1067a21373049f5a24c5a091f763de813eb2
Chandika Khatri <chandikakhatri117@gmail.com>
2022-03-15 22:56:26 +0545

    files deleted

--- src/Helpers/uploadPictureMulter.js
+++ src/Helpers/uploadPictureMulter.js
@@ -1,19 +1,19 @@
-const multer = require('multer');
-const path = require('path');
+// const multer = require('multer');
+// const path = require('path');
 
-var storage = multer.diskStorage({
-    destination: ( req, res, cb ) => {
-        cb(null, 'src/Uploads/Images')
-    },
-    filename: ( req, file, cb ) => {
-        cb( null, file.fieldname + '-' + Date.now() + path.extname(file.originalname) )
-    }
-});
-
-
-const uploadPicture = multer({ storage: storage });
+// var storage = multer.diskStorage({
+//     destination: ( req, res, cb ) => {
+//         cb(null, 'src/Uploads/Images')
+//     },
+//     filename: ( req, file, cb ) => {
+//         cb( null, file.fieldname + '-' + Date.now() + path.extname(file.originalname) )
+//     }
+// });
 
 
-module.exports = {
-    uploadPicture
-}
+// const uploadPicture = multer({ storage: storage });
+
+
+// module.exports = {
+//     uploadPicture
+// }
