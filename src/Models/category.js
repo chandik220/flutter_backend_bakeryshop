9a4a80f4beaae6040fd53bc7224080fa61686653
Chandika Khatri <chandikakhatri117@gmail.com>
2022-03-19 13:01:08 +0545

    somes files deleted

--- src/Models/category.js
+++ src/Models/category.js
@@ -1,21 +1,21 @@
 
-// const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-// const categorySchema = new Schema({
-//     category: {
-//         type: String,
-//         required: true
-//     },
-//     picture: {
-//         type: String,
-//         required: false
-//     },
-//     status: {
-//         type: Boolean,
-//         default: true,
-//         required: true
-//     }
-// });
+const categorySchema = new Schema({
+    category: {
+        type: String,
+        required: true
+    },
+    picture: {
+        type: String,
+        required: false
+    },
+    status: {
+        type: Boolean,
+        default: true,
+        required: true
+    }
+});
 
-// module.exports = model('category', categorySchema );
+module.exports = model('category', categorySchema );
 
