1bb503e8fabf37587ff897555e27c21691aaf53d
Chandika Khatri <chandikakhatri117@gmail.com>
2022-03-27 18:32:57 +0545

    some changes

--- src/Database/Mongodb.js
+++ src/Database/Mongodb.js
@@ -1,19 +1,19 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost:27017/products', {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-//     useFindAndModify:false,
-//     useCreateIndex:true,
+mongoose.connect('mongodb://localhost:27017/products', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify:false,
+    useCreateIndex:true,
    
-// }).then( db => console.log('Mongodb is connnected'))
-// .catch(err => console.log(err));
+}).then( db => console.log('Mongodb is connnected'))
+.catch(err => console.log(err));
 
 
-// // mongoose.connect('mongodb://localhost:27017/products', (err, res) => {
+// mongoose.connect('mongodb://localhost:27017/products', (err, res) => {
 
-// //     if( err ) throw err;
+//     if( err ) throw err;
 
-// //     console.log('Mongodb is connected');
+//     console.log('Mongodb is connected');
 
-// // });
+// });
